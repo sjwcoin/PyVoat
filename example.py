7a74c2d7d13971890030672726e9fd529f7871d1
Healdb <healdb@healdb.com>
2015-06-18 03:57:04 -0400

    Create example.py

---
+++ example.py
@@ -0,0 +1,23 @@
+import json
+import PyVoat
+
+v = PyVoat.PyVoat()
+
+messages = v.getAllMessages()
+user = v.getUser("healdb")
+subverse = v.getSubverse("api")
+
+#Loop through all unread messages
+for message in messages:
+  print(message.author)
+  print(message.body)
+
+print(user.getInfo())
+
+#Loops through each post in the subverse
+submissions = subverse.getSubmissions()
+for post in submissions:
+  print(post.author)
+  comments= post.getComments()
+  for comment in comments:
+    print(comment.body)
