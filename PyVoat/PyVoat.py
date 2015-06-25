29546ada9872fc2b67f053af717203b14975bd7d
Healdb <healdb@healdb.com>
2015-06-25 02:34:11 -0400

    Removed need for outside config file

--- PyVoat/PyVoat.py
+++ PyVoat/PyVoat.py
@@ -1,45 +1,45 @@
 import requests
 import json
 import objects
+
 frontpoint = "https://fakevout.azurewebsites.net/api/"
+
 class PyVoat:
-    def __init__(self):
-        f=open("config.txt")
-        lines=f.readlines()
-        trash, token = lines[0].split(":")
-        trash, pubkey = lines[1].split(":")
-        self.headers = {'content-type': 'application/json', 'Authorization': "Bearer " + token.rstrip(),'Voat-ApiKey': pubkey.rstrip()}
+    def __init__(self,token,pubkey):
+        self.token = token
+        self.pubkey = pubkey
+        self.headers = {'content-type': 'application/json', 'Authorization': "Bearer " + self.token,'Voat-ApiKey': self.pubkey}
     def getUser(self,username):
-        user = objects.user(username)
+        user = objects.user(self.token,self.pubkey,username)
         return user
     def getSubverse(self, subverse):
-        subverse = objects.subverse(subverse)
+        subverse = objects.subverse(self.token,self.pubkey,subverse)
         return subverse
     def getSubmissionById(self,submis):
-        response = requests.get(frontpoint + "v1/submissions/"+submis, headers=self.headers)
+        response = requests.get(frontpoint + "v1/submissions/"+str(submis), headers=self.headers)
         json_input = response.json()
         decoded = json.dumps(json_input)
         decoded = json.loads(decoded)
         data = decoded['data']
-        submission = objects.submission(data['subverse'],data['title'],data['userName'],data['url'],data['content'],submis)
+        submission = objects.submission(self.token,self.pubkey,data['subverse'],data['title'],data['userName'],data['url'],data['content'],submis)
         return submission
     def getCommentById(self,comid):
-        response = requests.get(frontpoint + "v1/comments/"+comid, headers=self.headers)
+        response = requests.get(frontpoint + "v1/comments/"+str(comid), headers=self.headers)
         json_input = response.json()
         decoded = json.dumps(json_input)
         decoded = json.loads(decoded)
         data = decoded['data']
-        comment = objects.comment(comid, data['subverse'], data['userName'], data['content'], data['parentID'])
+        comment = objects.comment(self.token,self.pubkey,comid, data['subverse'], data['userName'], data['content'], data['parentID'], data['submissionID'])
         return comment
     def getUsernameMentions(self):
-        response = requests.get(frontpoint + "v1/u/messages/Mention/unread", headers=self.headers)
+        response = requests.get(frontpoint + "v1/u/messages/mention/unread", headers=self.headers)
         json_input = response.json()
         decoded = json.dumps(json_input)
         decoded = json.loads(decoded)
         data = decoded['data']
         messages=[]
         for x in data:
-            message = objects.message(x["id"],x["sender"],x["content"])
+            message = objects.message(self.token,self.pubkey,x["commentID"],x["sender"],x["content"])
             messages.append(message)
         return messages
     def getPrivateMessages(self):
@@ -50,7 +50,7 @@ class PyVoat:
         data = decoded['data']
         messages=[]
         for x in data:
-            message = objects.message(x["id"],x["sender"],x["content"])
+            message = objects.message(self.token,self.pubkey,x["id"],x["sender"],x["content"])
             messages.append(message)
         return messages
     def getAllMessages(self):
@@ -61,7 +61,7 @@ class PyVoat:
         data = decoded['data']
         messages=[]
         for x in data:
-            message = objects.message(x["id"],x["sender"],x["content"])
+            message = objects.message(self.token,self.pubkey,x["id"],x["sender"],x["content"])
             messages.append(message)
         return messages
     def sendMessage(self,user,subject,body):
