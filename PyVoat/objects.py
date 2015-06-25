2fe120504a33fe6c638b0c8d987b6b25eae23da1
Healdb <healdb@healdb.com>
2015-06-25 02:33:13 -0400

    Removed need for outside config file

--- PyVoat/objects.py
+++ PyVoat/objects.py
@@ -3,16 +3,16 @@ import json
 import time
 import PyVoat
 
-f=open("config.txt")
-lines=f.readlines()
-trash, token = lines[0].split(":")
-trash, pubkey = lines[1].split(":")
-headers = {'content-type': 'application/json', 'Authorization': 'Bearer ' +token.rstrip(),'Voat-ApiKey': pubkey.rstrip()}
 frontpoint = "https://fakevout.azurewebsites.net/api/v1/"
 
 class submission:
-    def __init__(self,sub,title,username,url,body,subid):
+    def __init__(self,token,pubkey,sub,title,username,url,body,subid):
+        self.token=token
+        self.pubkey=pubkey
         self.subverse = sub
+        self.headers = {'content-type': 'application/json', 'Authorization': 'Bearer ' +token,'Voat-ApiKey': pubkey}
+        self.token=token
+        self.pubkey=pubkey
         self.title = title
         self.id = subid
         self.author =username
@@ -20,15 +20,15 @@ class submission:
         self.body=body
     def postSubmissionReply(self,value):
         post_data = json.dumps({'value': value})
-        response = requests.post(frontpoint + "v/"+self.subverse+"/"+self.id+"/comment", data=post_data, headers=headers)
+        response = requests.post(frontpoint + "v/"+self.subverse+"/"+str(self.id)+"/comment", data=post_data, headers=self.headers)
         json_input = response.json()
         decoded = json.dumps(json_input)
         decoded = json.loads(decoded)
         data = decoded['data']
         return data
     def getComments(self):
-        v = PyVoat.PyVoat()
-        response = requests.get(frontpoint + "v/"+self.subverse+"/"+self.id+"/comments", headers=headers)
+        v = PyVoat.PyVoat(self.token,self.pubkey)
+        response = requests.get(frontpoint + "v/"+self.subverse+"/"++str(self.id)+"/comments", headers=self.headers)
         json_input = response.json()
         decoded = json.dumps(json_input)
         decoded = json.loads(decoded)
@@ -41,13 +41,16 @@ class submission:
             time.sleep(2)
         return comments
     def delete(self):
-        response = requests.delete(frontpoint + "v/"+self.subverse+"/"+self.id,headers=headers)
+        response = requests.delete(frontpoint + "v/"+self.subverse+"/"+str(self.id),headers=self.headers)
         return response
 class user:
-    def __init__(self,user):
+    def __init__(self,token,pubkey,user):
+        self.token=token
+        self.pubkey=pubkey
         self.user = user
+        self.headers = {'content-type': 'application/json', 'Authorization': 'Bearer ' +token,'Voat-ApiKey': pubkey}
     def getInfo(self):
-        response = requests.get(frontpoint + "u/"+self.user+"/info", headers=headers)
+        response = requests.get(frontpoint + "u/"+self.user+"/info", headers=self.headers)
         json_input = response.json()
         decoded = json.dumps(json_input)
         decoded = json.loads(decoded)
@@ -55,7 +58,7 @@ class user:
         data = decoded['data']
         return data
     def getPrefs(self):
-        response = requests.get(frontpoint + "u/preferences", headers=headers)
+        response = requests.get(frontpoint + "u/preferences", headers=self.headers)
         json_input = response.json()
         decoded = json.dumps(json_input)
         decoded = json.loads(decoded)
@@ -63,8 +66,8 @@ class user:
         data = decoded['data']
         return data
     def getComments(self):
-        v = PyVoat.PyVoat()
-        response = requests.get(frontpoint + "u/"+self.user+"/comments", headers=headers)
+        v = PyVoat.PyVoat(self.token,self.pubkey)
+        response = requests.get(frontpoint + "u/"+self.user+"/comments", headers=self.headers)
         json_input = response.json()
         decoded = json.dumps(json_input)
         decoded = json.loads(decoded)
@@ -77,8 +80,8 @@ class user:
             time.sleep(2)
         return comments
     def getSubmissions(self):
-        v = PyVoat.PyVoat()
-        response = requests.get(frontpoint + "u/"+self.user+"/submissions", headers=headers)
+        v = PyVoat.PyVoat(self.token,self.pubkey)
+        response = requests.get(frontpoint + "u/"+self.user+"/submissions", headers=self.headers)
         json_input = response.json()
         decoded = json.dumps(json_input)
         decoded = json.loads(decoded)
@@ -91,36 +94,43 @@ class user:
             time.sleep(2)
         return posts
 class comment:
-    def __init__(self,comid,subverse,author,body,parent):
+    def __init__(self,token,pubkey,comid,subverse,author,body,parent,postid):
+        self.token=token
+        self.pubkey=pubkey
+        self.headers = {'content-type': 'application/json', 'Authorization': 'Bearer ' +token,'Voat-ApiKey': pubkey}
         self.id =comid
         self.subverse=subverse
         self.author=author
         self.body=body
         self.parent = parent
+        self.postID =postid
     def postCommentReply(self,value):
         post_data = json.dumps({'value': value})
-        response = requests.post(frontpoint + "comments/"+self.id, data=post_data, headers=headers)
+        response = requests.post(frontpoint + "comments/"+str(self.id), data=post_data, headers=self.headers)
         json_input = response.json()
         decoded = json.dumps(json_input)
         decoded = json.loads(decoded)
         data = decoded['data']
         return data
     def delete(self):
-        response = requests.delete(frontpoint + "comments/"+self.id,headers=headers)
+        response = requests.delete(frontpoint + "comments/"+str(self.id),headers=self.headers)
         return response
 class subverse:
-    def __init__(self,subverse):
+    def __init__(self,token,pubkey,subverse):
+        self.token=token
+        self.pubkey=pubkey
         self.subverse = subverse
+        self.headers = {'content-type': 'application/json', 'Authorization': 'Bearer ' +token,'Voat-ApiKey': pubkey}
     def info(self):
-        response = requests.get(frontpoint + "v/"+self.subverse+"/info", headers=headers)
+        response = requests.get(frontpoint + "v/"+self.subverse+"/info", headers=self.headers)
         json_input = response.json()
         decoded = json.dumps(json_input)
         decoded = json.loads(decoded)
         data = decoded['data']
         return data
     def getSubmissions(self):
-        v = PyVoat.PyVoat()
-        response = requests.get(frontpoint + "v/"+self.subverse, headers=headers)
+        v = PyVoat.PyVoat(self.token,self.pubkey)
+        response = requests.get(frontpoint + "v/"+self.subverse, headers=self.headers)
         json_input = response.json()
         decoded = json.dumps(json_input)
         decoded = json.loads(decoded)
@@ -133,8 +143,8 @@ class subverse:
             time.sleep(2)
         return posts
     def getComments(self):
-        v = PyVoat.PyVoat()
-        response = requests.get(frontpoint + "v/"+self.subverse, headers=headers)
+        v = PyVoat.PyVoat(self.token,self.pubkey)
+        response = requests.get(frontpoint + "v/"+self.subverse, headers=self.headers)
         json_input = response.json()
         decoded = json.dumps(json_input)
         decoded = json.loads(decoded)
@@ -142,7 +152,7 @@ class subverse:
         allData =[]
         for x in data:
             subid= x['id']
-            response = requests.get(frontpoint + "v/"+self.subverse+"/"+str(subid)+"/comments", headers=headers)
+            response = requests.get(frontpoint + "v/"+self.subverse+"/"+str(subid)+"/comments", headers=self.headers)
             json_input = response.json()
             decoded = json.dumps(json_input)
             decoded = json.loads(decoded)
@@ -159,7 +169,7 @@ class subverse:
     def postSelfSubmission(self,title,content):
         post_data = json.dumps({'title': title, 'content': content})
         print(post_data)
-        response = requests.post(frontpoint + "v/"+self.subverse, data=post_data, headers=headers)
+        response = requests.post(frontpoint + "v/"+self.subverse, data=post_data, headers=self.headers)
         json_input = response.json()
         decoded = json.dumps(json_input)
         decoded = json.loads(decoded)
@@ -167,20 +177,23 @@ class subverse:
         return data
     def postLinkSubmission(self,title,url):
         post_data = json.dumps({'title': title, 'url': url})
-        response = requests.post(frontpoint + "v/"+self.subverse, data=post_data, headers=headers)
+        response = requests.post(frontpoint + "v/"+self.subverse, data=post_data, headers=self.headers)
         json_input = response.json()
         decoded = json.dumps(json_input)
         decoded = json.loads(decoded)
         data = decoded['data']
         return data
 class message:
-    def __init__(self,mesID, username, content):
+    def __init__(self,token,pubkey,mesID, username, content):
+        self.token=token
+        self.pubkey=pubkey
+        self.headers = {'content-type': 'application/json', 'Authorization': 'Bearer ' +token,'Voat-ApiKey': pubkey}
         self.id = mesID
         self.author = username
         self.body = content
     def reply(self,text):
         post_data = json.dumps({'value': text})
-        response = requests.post(frontpoint + "u/messages/reply/"+str(self.id), data=post_data, headers=headers)
+        response = requests.post(frontpoint + "u/messages/reply/"+str(self.id), data=post_data, headers=self.headers)
         json_input = response.json()
         decoded = json.dumps(json_input)
         decoded = json.loads(decoded)
@@ -188,4 +201,3 @@ class message:
         data = decoded['data']
         return data
         
-    
